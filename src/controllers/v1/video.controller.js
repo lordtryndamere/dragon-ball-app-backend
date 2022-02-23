1ac6934f3e842f506079eac7aaf148f88348f0db
FlutterAwesome <flutterawesome@iMac-Pro-de-FlutterAwesome.local>
2022-02-22 21:57:11 -0500

    fixed video get

--- src/controllers/v1/video.controller.js
+++ src/controllers/v1/video.controller.js
@@ -2,18 +2,15 @@ const fs = require('fs');
 const path = require('path')   
 class VideoController {
     getVideo = async (req, res) => {
-        try {
             const videoId = req.params.videoId;
-            const videoPath = path.join(__dirname, '../../../uploads/'+videoId);
-            const headers = {
-              "Content-Type": "video/mp4",
-            };
-            res.writeHead(200, headers);
-            const videoStream = fs.createReadStream(videoPath);
-            videoStream.pipe(res);
-        } catch (error) {
-            return res.status(500).send(error)
-        }
+            const mimetype = nameImage.split('.')[1];
+            fs.readFile(path.join(__dirname, '../../../uploads/'+videoId),(err,data)=>{
+                if(err) return res.status(500).send({err})
+                res.writeHead(200,{'Content-type':`video/${mimetype}`})
+                res.write(data);
+                res.end();
+            });
+     
     }
 
     getImage = async (req,res)=>{
