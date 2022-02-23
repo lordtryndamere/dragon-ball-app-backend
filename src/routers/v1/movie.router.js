83246d32d38ff9d31a5ff57ca64a9b8da49c9397
FlutterAwesome <flutterawesome@iMac-Pro-de-FlutterAwesome.local>
2022-02-23 18:35:26 -0500

    finish total movies section

--- src/routers/v1/movie.router.js
+++ src/routers/v1/movie.router.js
@@ -4,9 +4,9 @@ const movie = new MovieController();
 const { vertifyTokenMiddleware } = require('../../middlewares/verify-token.middleware')
 module.exports = () => {
     const router = Router();
-    router.get('/get-by-category/:categoryId', vertifyTokenMiddleware('access'), movie.getMoviesByCategory);
-    router.get('/get-categories', vertifyTokenMiddleware('access'), movie.getCategories);
-    router.post('/get-movies', vertifyTokenMiddleware('access'), movie.getMovies);
+    router.get('/get-by-category/:categoryId', movie.getMoviesByCategory);
+    router.get('/get-categories', movie.getCategories);
+    router.post('/get-movies',  movie.getMovies);
     router.post('/create-movie', vertifyTokenMiddleware('access'), movie.createMovie);
     router.post('/create-category', vertifyTokenMiddleware('access'), movie.createCategory);
 
