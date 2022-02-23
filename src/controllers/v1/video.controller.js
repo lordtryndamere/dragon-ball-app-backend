eaa4a766a46d0cbd4e6d1da779fc424759b69219
FlutterAwesome <flutterawesome@iMac-Pro-de-FlutterAwesome.local>
2022-02-22 21:20:44 -0500

    fixed get video get

--- src/controllers/v1/video.controller.js
+++ src/controllers/v1/video.controller.js
@@ -2,7 +2,30 @@ const fs = require('fs');
 const path = require('path')   
 class VideoController {
     getVideo = async (req, res) => {
-   
+        try {
+            const range = req.headers.range;
+            const videoId = req.params.videoId;
+            if (!range) {
+              res.status(400).send("Range not provided");
+            }
+            const videoPath = path.join(__dirname, '../../../uploads/'+videoId);
+            const videoSize = fs.statSync(videoPath).size;
+            const chunkSize = 10 ** 6; // 1 mb
+            const start = Number(range.replace(/\D/g, ""));
+            const end = Math.min(start + chunkSize, videoSize - 1);
+            const contentLength = end - start + 1;
+            const headers = {
+              "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+              "Accept-Ranges": "bytes",
+              "Content-Length": contentLength,
+              "Content-Type": "video/mp4",
+            };
+            res.writeHead(206, headers);
+            const videoStream = fs.createReadStream(videoPath, { start, end });
+            videoStream.pipe(res);
+        } catch (error) {
+            return res.status(500).send(error)
+        }
         const range = req.headers.range;
         const videoId = req.params.videoId;
         if (!range) {
